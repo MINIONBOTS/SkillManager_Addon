133804ad57615fd156fba9fa79ae42ce0d243b06
fxfire <minionoffice@gmail.com>
2020-02-12 10:47:11 +0100

    fixed ranger n getcompletespellinfo

--- sm_profile.lua
+++ sm_profile.lua
@@ -84,12 +84,7 @@ function sm_profile:UpdateContext()
 	self.temp.skillstopsmovement = false
 	self.temp.context.actionlist = self.actionlist
 	local allskills = Player:GetCompleteSpellInfo()
-	if ( allskills ) then
-		if ( Player.profession == GW2.CHARCLASS.Ranger
-			and allskills[GW2.SKILLBARSLOT.Slot_12] == nil) then
-			-- TODO: Temporary fix pet skills, remove when Player:GetCompleteSpellInfo() contains it!
-			allskills[GW2.SKILLBARSLOT.Slot_12] = Player:GetSpellInfo(GW2.SKILLBARSLOT.Slot_12)
-		end
+	if ( allskills ) then		
 		self.temp.context.skillbar = allskills
 	end
 
